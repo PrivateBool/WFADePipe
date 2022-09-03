6422ce8d9e0645372ba4230a76bf247d1e68686c
PrivateCoolean <mathieu_collin@hotmail.com>
2022-09-03 08:38:02 -0400

    Premier commit

---
+++ WFADePipe/De.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WFADePipe
+{
+    public abstract class De
+    {
+        private int valeur;
+        private readonly int nombreDeFace = 6;
+        private readonly bool estPipe;
+        public Random random = new Random();
+        public int NomBreDeFace => nombreDeFace;
+
+        public int Valeur { get => valeur; set => valeur = value; }
+
+        public De(bool estPipe)
+        {
+            this.estPipe = estPipe;
+        }
+        public virtual void Brasser(De DeABrasser)
+        {
+            DeABrasser.Valeur = (int)(random.NextDouble() * DeABrasser.NomBreDeFace);
+        }
+    }
+}
---
+++ WFADePipe/DePiPe.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WFADePipe
+{
+    public class DePiPe: De
+    {
+        public DePiPe() : base(true)
+        {
+
+        }
+        public override void Brasser(De DeABrasser)
+        {
+            var resultatDe = (int)(random.NextDouble() * DeABrasser.NomBreDeFace*2);
+            if (resultatDe > 6)
+            {
+                DeABrasser.Valeur = 6;
+            }
+            else
+            {
+                DeABrasser.Valeur = resultatDe;
+            }
+        }
+    }
+}
